1a1bf44cdbac3011e960d663f6af2605981ffd37
Jossel Redoquerio <josselgredoquerio@iskolarngbayan.pup.edu.ph>
2024-01-28 14:19:49 +0800

    difference of frame1 and frame2

--- build_security_camera.py
+++ build_security_camera.py
@@ -2,7 +2,9 @@ import cv2
 
 cam = cv2.VideoCapture(0)
 while cam.isOpened():
-    ret, frame = cam.read()
+    ret, frame1 = cam.read()
+    ret, frame2 = cam.read()
+    diff = cv2.absdiff(frame1, frame 2)
     if cv2.waitKey(10) == ord('q'):
         break
-    cv2.imshow('Jossel Security Cam', frame)
+    cv2.imshow('Jossel Security Cam', diff)
